d783139d16d17fca29e8e26cca5fb0fd35d19d70
Sas <sasoon91@gmail.com>
2023-04-26 23:11:07 +1000

    updates..

--- main.js
+++ main.js
@@ -3,34 +3,47 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-
 const renderer = new THREE.WebGLRenderer();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const controls = new OrbitControls(camera, renderer.domElement);
 
-
 // Define your scenes
 const scene1 = new THREE.Scene();
-addLighting(scene1);
 const scene2 = new THREE.Scene();
-addLighting(scene2);
 
 let currentScene = scene1; // Set default scene
-
-function addLighting(scene) {
-  // Ambient light
-  const ambientLight = new THREE.AmbientLight(0xffffff, 3);
-  scene.add(ambientLight);
-
-  // Directional light
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 7);
-  directionalLight.position.set(0, 10, 0);
-  scene.add(directionalLight);
-}
 
 // Enable shadows for objects that receive shadows
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.useLegacyLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+// Set up the renderer with tone mapping
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1.0; // Adjust this value to control the exposure
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Add lighting
+function addLights(scene) {
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.position.set(5, 10, 5);
+  directionalLight.target.position.set(0, 0, 0);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 512;
+  directionalLight.shadow.mapSize.height = 512;
+  directionalLight.shadow.camera.near = 1;
+  directionalLight.shadow.camera.far = 50;
+  scene.add(directionalLight);
+  scene.add(directionalLight.target);
+}
+
+addLights(scene1);
+addLights(scene2);
 
 // Set up camera and controls
 camera.position.set(0, 1, 3);
@@ -40,12 +53,7 @@ const loader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderConfig({ type: 'js' });
 dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-loader.setDRACOLoader( dracoLoader ); // Pass the DRACOLoader instance to the GLTFLoader
-
-
-
-
-// ... Your other imports and setup code ...
+loader.setDRACOLoader(dracoLoader); // Pass the DRACOLoader instance to the GLTFLoader
 
 loader.load('https://interactivehouse.s3.ap-southeast-2.amazonaws.com/house.glb', (gltf) => {
   scene1.add(gltf.scenes[0]); // Add the first child of the root object to the scene
@@ -64,24 +72,28 @@ loader.load('https://interactivehouse.s3.ap-southeast-2.amazonaws.com/house.glb'
   console.error(error);
 });
 
-// ... The rest of your code ...
-
-
-
-
 const sceneSelect = document.querySelector('#scene-select');
-sceneSelect.addEventListener('change', (event) => {
+sceneSelect.addEventListener('change', handleSceneChange);
+
+function handleSceneChange(event) {
   const selectedScene = event.target.value;
   currentScene.visible = false;
   currentScene = selectedScene === '0' ? scene1 : scene2;
   currentScene.visible = true;
-});
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(currentScene, camera);
 }
-animate();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+animate();
 document.body.appendChild(renderer.domElement);
